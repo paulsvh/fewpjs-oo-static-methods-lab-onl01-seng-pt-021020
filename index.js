f257ab790ba6d83f76d538359936df7a3795bbfe
Paul von Hagen <pvonhagen26@gmail.com>
2020-09-08 01:57:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string){
+    let nonCapitalized = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = []
+    let splitString = string.split(" ")
+    for (let i=0; i < splitString.length; i++){
+      if (i === 0) {
+        result.push(this.capitalize(splitString[i]))
+      }
+      else {
+        if (nonCapitalized.includes(splitString[i])){
+          result.push(splitString[i])
+        }
+        else {
+          result.push(this.capitalize(splitString[i]))
+        }
+      }
+    }
+    return result.join(" ");
+  }
+
 }
